4109880a6d8bb2c2de78e49102aed1c6e74a5dff
Akash Anand <akashanand.iitd@gmail.com>
2023-10-14 21:06:26 +0530

    Remove default auth
    Signed-off-by: Akash Anand <akashanand.iitd@gmail.com>

--- chat-app/src/contexts/AuthContext.js
+++ chat-app/src/contexts/AuthContext.js
@@ -10,13 +10,7 @@ export const AuthProvider = ({ children }) => {
     //const [isLoggedIn, setIsLoggedIn] = useState(false);
     //const [userInfo, setUserInfo] = useState({})
     const [isLoggedIn, setIsLoggedIn] = useState(true);
-    const [userInfo, setUserInfo] = useState({
-        "name": "Akash Anand",
-        "email": "akashanand.iitd@gmail.com",
-        "family_name": "Anand",
-        "given_name": "Akash",
-        "picture": "https://lh3.googleusercontent.com/a/ACg8ocJ1556xpPhCZvGrbGtcu7onwe7ICMeOWprJAY5US5mh_DU=s96-c"
-    })
+    const [userInfo, setUserInfo] = useState({})
 
     const login = (userInfo) => {
         setIsLoggedIn(true);
