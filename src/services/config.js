43b92b1901bbfecfe720427af62f4cd0004be35d
Akash Anand <akashanand.iitd@gmail.com>
2023-10-30 15:11:07 +0530

    Add customized deployment routes

--- src/services/config.js
+++ src/services/config.js
@@ -1,3 +1,19 @@
-export const BASE_URL = "https://tailortalk-production.up.railway.app";
+// export const BASE_URL = "https://tailortalk-production.up.railway.app";
 // export const BASE_URL = "https://tailortalk-preview.up.railway.app";
 // export const BASE_URL = "http://localhost:8000";
+let BASE_URL;
+console.log("REACT_APP_ENV_NAME is: ", process.env.REACT_APP_ENV_NAME)
+
+switch (process.env.REACT_APP_ENV_NAME) {
+    case 'production':
+        BASE_URL = "https://tailortalk-production.up.railway.app";
+        break;
+    case 'preview':
+        BASE_URL = "https://tailortalk-preview.up.railway.app";
+        break;
+    default:
+        BASE_URL = "http://localhost:8000";
+        break;
+}
+
+export { BASE_URL };
