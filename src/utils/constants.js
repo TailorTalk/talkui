e8406bb5ce72bbcba5180bbef3fd4d1744d4442d
Akash Anand <akashanand.iitd@gmail.com>
2023-11-04 19:15:54 +0530

    Add firebase auth

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,11 +3,11 @@ export const ASSET_NAME_CLASS_MAP = {
 }
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyD0Veq9Ot3C_64U2lTd3cIDfqbAgwECwls",
-    authDomain: "chat-app-400104.firebaseapp.com",
-    projectId: "chat-app-400104",
-    storageBucket: "chat-app-400104.appspot.com",
-    messagingSenderId: "605633798909",
-    appId: "1:605633798909:web:61d2e04c9226c5677ed494",
-    measurementId: "G-7WN5PKX8G8"
+    apiKey: process.env.REACT_APP_FIREBASE__apiKey,
+    authDomain: process.env.REACT_APP_FIREBASE__authDomain,
+    projectId: process.env.REACT_APP_FIREBASE__projectId,
+    storageBucket: process.env.REACT_APP_FIREBASE__storageBucket,
+    messagingSenderId: process.env.REACT_APP_FIREBASE__messagingSenderId,
+    appId: process.env.REACT_APP_FIREBASE__appId,
+    measurementId: process.env.REACT_APP_FIREBASE__measurementId
   };
