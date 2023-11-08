e4a3b70d8c216a09052a8964f190b81b8911b767
Akash Anand <akashanand.iitd@gmail.com>
2023-11-08 23:56:25 +0530

    Notion of collaborator

--- src/services/orgs.service.js
+++ src/services/orgs.service.js
@@ -17,6 +17,16 @@ class OrgsService {
             }
         });
     }
+
+    addCollaborator(orgName, collaboratorId) {
+        return http.post("/maestro_chat/org/v1/add_collaborator", {
+            "org_name": orgName,
+            "collaborator_id": collaboratorId
+        }, {
+            headers: {
+            }
+        });
+    }
 }
 
 const orgsService = new OrgsService();
