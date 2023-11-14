0e06e8f90c4abdd433103aee7b95625d5eca3c3a
Akash Anand <akashanand.iitd@gmail.com>
2023-11-15 00:00:20 +0530

    Generic asset initial

---
+++ src/components/AssetClass/GenericAsset/assetValidator.js
@@ -0,0 +1,23 @@
+const fields = [
+    "asset_class",
+    "asset_tool_name",
+    "asset_tool_short_description",
+    "asset_tool_description",
+    "function_schema",
+    "output_schema",
+    "run_instruction",
+    "model_name"
+]
+
+function validateGenericAsset(asset) {
+    console.log("Asset in validateTextAsset: ", asset)
+    let errors = ""
+    fields.forEach((field) => {
+        if (!asset[field]) {
+            errors += field + " is required\n"
+        }
+    })
+    return errors
+}
+
+export default validateGenericAsset
