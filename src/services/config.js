5fed24a6c072716ab1a8882dc6f3b6c8290ece7e
shiva <shivajbd@gmail.com>
2023-12-27 19:23:34 +0530

    config changes

--- src/services/config.js
+++ src/services/config.js
@@ -2,17 +2,21 @@
 // export const BASE_URL = "https://tailortalk-preview.up.railway.app";
 // export const BASE_URL = "http://localhost:8000";
 let BASE_URL;
+let BASE_URL_CONSOLE;
 // console.log("REACT_APP_ENV_NAME is: ", process.env.REACT_APP_ENV_NAME)
 
 switch (process.env.REACT_APP_ENV_NAME?process.env.REACT_APP_ENV_NAME.toLowerCase(): "default") {
     case 'production':
         BASE_URL = "https://tailortalk-production.up.railway.app";
+        BASE_URL_CONSOLE = "https://externalchatplugins-production.up.railway.app/";
         break;
     case 'preview':
         BASE_URL = "https://tailortalk-preview.up.railway.app";
+        BASE_URL_CONSOLE = "https://externalchatplugins-preview.up.railway.app/";
         break;
     default:
         BASE_URL = "https://tailortalk-preview.up.railway.app";
+        BASE_URL_CONSOLE = "https://externalchatplugins-preview.up.railway.app/";
         break;
 }
 
